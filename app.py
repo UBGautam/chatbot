c8084755fb46ae8386038a20ffb7c8e3076ebfa5
UB Gautam <33533804+UBGautam@users.noreply.github.com>
2023-11-10 21:39:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def communicate():
     response = client.chat.completions.create(model="gpt-3.5-turbo",
     messages=messages
     )
-    bot_message = response.choices[0].message.content
+    bot_message = response["choices"][0]["message"]
     messages.append(bot_message)
     
     st.session_state["user_input"] = ""
