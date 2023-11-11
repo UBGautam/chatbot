48f40564f70787dd8cfa530f55ed50b247647bcc
UB Gautam <33533804+UBGautam@users.noreply.github.com>
2023-11-11 14:10:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ if "messages" not in st.session_state:
         {"role": "system", "content": "You are a trip adviser assistant AI."}
     ]
 
+
 def communicate():
     messages = st.session_state["messages"]
 
@@ -17,10 +18,11 @@ def communicate():
     response = client.chat.completions.create(model="gpt-3.5-turbo",
                                               messages=messages
                                               )
-    bot_message = response.choices[0].message
-    messages.append(bot_message)
+    bot_message = response.choices[0]
+    messages.append({"role": "assistant", "content": bot_message.content})
 
     st.session_state["user_input"] = ""
+
 
 st.title("Trip Adviser AI")
 st.write("Utilizing the ChatGPT API, this chatbot offers advanced conversational capabilities.")
@@ -32,10 +34,18 @@ if st.session_state["messages"]:
 
     for message in reversed(messages[1:]):
         speaker = "🙂"
-        if message["role"] == "assistant":
+        if hasattr(message, "role") and message.role == "assistant":
             speaker = "🤖"
 
         # Extract the message content from the ChatCompletionMessage
-        message_content = message["content"] if isinstance(message, dict) else message
+        if isinstance(message, dict):
+            if message["role"] == "assistant":
+                speaker = "🤖"
+                message_content = message["content"]
+            else:
+                speaker = "🙂"
+                message_content = message["content"]
+        else:
+            message_content = message
 
         st.write(speaker + ": " + message_content)
