c1a643ae701aa23a0497bdde73bc0f96e28df155
UB Gautam <33533804+UBGautam@users.noreply.github.com>
2023-11-11 16:41:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ if "messages" not in st.session_state:
         {"role": "system", "content": "You are a trip adviser assistant AI."}
     ]
 
+
 def communicate():
     messages = st.session_state["messages"]
 
@@ -15,23 +16,12 @@ def communicate():
     messages.append(user_message)
 
     response = client.chat.completions.create(model="gpt-3.5-turbo",
-                                               messages=messages)
-    bot_message = response.choices[0]
-
-    # Check for 'text' attribute instead of 'content'
-    if hasattr(bot_message, 'text'):
-        message_content = bot_message.text
-    else:
-        # Handle the case where neither 'content' nor 'text' exists
-        raise AttributeError("Unable to extract message content from bot response")
-
-    # Format the bot's response with the desired emoji
-    formatted_response = "🤖: " + message_content
-
-    messages.append({"role": "assistant", "content": formatted_response})
+                                              messages=messages
+                                              )
+    bot_message = response.choices[0].message
+    messages.append(bot_message)
 
     st.session_state["user_input"] = ""
-
 
 
 st.title("Trip Adviser AI")
