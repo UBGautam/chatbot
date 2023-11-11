fed0c02aeee83bf720564a39b45cc7af254d9dc6
UB Gautam <33533804+UBGautam@users.noreply.github.com>
2023-11-11 15:06:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,19 +16,12 @@ def communicate():
     messages.append(user_message)
 
     response = client.chat.completions.create(model="gpt-3.5-turbo",
-                                               messages=messages)
-    bot_message = response.choices[0]
-
-    # Extract the message content from the ChatCompletionMessage object
-    message_content = bot_message.content
-
-    # Format the response with the desired emoji and message content
-    formatted_response = "🤖: " + message_content
-
-    messages.append({"role": "assistant", "content": formatted_response})
+                                              messages=messages
+                                              )
+    bot_message = response.choices[0].message
+    messages.append(bot_message)
 
     st.session_state["user_input"] = ""
-
 
 
 st.title("Trip Adviser AI")
