b3572623230a681412f91bda81003d9e781ec951
UB Gautam <33533804+UBGautam@users.noreply.github.com>
2023-11-11 13:41:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -35,10 +35,10 @@ user_input = st.text_input("please enter a message here.", key="user_input", on_
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
 
-    for message in reversed(messages[1:]):
+    for message in reversed(messages[1:]): 
         speaker = "🙂"
-        if message["role"] == "assistant":
+        if hasattr(message, "role") and message.role == "assistant":
             speaker="🤖"
 
-        #if hasattr(message, "content"):
-        st.write(speaker + ": " + message.content)
+        if hasattr(message, "content"):
+            st.write(speaker + ": " + message.content)
