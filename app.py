fc256afcc86bcac4fe86b73aacf3086f22f9b113
UB Gautam <33533804+UBGautam@users.noreply.github.com>
2023-11-11 15:25:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
+
 import streamlit as st
 from openai import OpenAI
 
 client = OpenAI(api_key=st.secrets.OpenAIAPI.openai_api_key)
+# Obtaining the OpenAI API key from Streamlit Community Cloud's 'Secrets'.
 
+# Using st.session_state to store the exchange of messages.
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
         {"role": "system", "content": "You are a trip adviser assistant AI."}
-    ]
+        ]
 
-
+# Function for interacting with a chatbot.
 def communicate():
     messages = st.session_state["messages"]
 
@@ -16,14 +19,15 @@ def communicate():
     messages.append(user_message)
 
     response = client.chat.completions.create(model="gpt-3.5-turbo",
-                                              messages=messages
-                                              )
+    messages=messages
+    )
     bot_message = response.choices[0].message
     messages.append(bot_message)
-
+    
     st.session_state["user_input"] = ""
 
 
+# User Interface
 st.title("Trip Adviser AI")
 st.write("Utilizing the ChatGPT API, this chatbot offers advanced conversational capabilities.")
 
@@ -32,15 +36,15 @@ user_input = st.text_input("please enter a message here.", key="user_input", on_
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
 
-    for message in reversed(messages[1:]):
+    for message in reversed(messages[1:]): 
         speaker = "🙂"
         if hasattr(message, "role") and message.role == "assistant":
-            speaker = "🤖"
+            speaker="🤖"
 
-        if isinstance(message, dict):
-            if message["role"] == "user":
-                st.write(speaker + ": " + message["content"])
-            else:
-                st.write(speaker + ": " + message.content)
-        else:
-            st.write(message)
+        if hasattr(message, "content"):
+            st.write(speaker + ": " + message.content)
+
+
+
+
+#ensuring that both user inputs and chatbot responses are displayed in the conversation history.
